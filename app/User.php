0d89c1c224f4d6441d570b274bf8ca257515144c
Colaborador PC-02 <noveltie.sac@gmail.com>
2016-12-05 13:17:53 -0500

    change email to username in password grant type

--- app/User.php
+++ app/User.php
@@ -36,4 +36,9 @@ class User extends Authenticatable
             $this->attributes['password'] = Hash::make($value);
         }
     }
+
+    public function findForPassport($username)
+    {
+        return $this->where('username', $username)->first();
+    }
 }
